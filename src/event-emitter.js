eada5886f3a2b87e1e52cc11a29a4797de0f201a
Your Name <you@example.com>
2019-09-17 22:29:07 -0700

    event emitter

--- src/event-emitter.js
+++ src/event-emitter.js
@@ -1,30 +1,18 @@
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var ab = new events.EventEmitter();
 
-var listner1 = () => console.log('listner1 executed.');
-var listner2 = () => console.log('listner2 executed.');
+ab.on('yoyoEvents', (a) => {
+   function vbg(){
+      setImmediate( () =>{
+         console.log('Set Imm');
+      })
+      console.log('start');
 
-// Bind the connection event with the listner1 function
-eventEmitter.addListener('connection', listner1);
+   }
+   process.nextTick(vbg);
 
-// Bind the connection event with the listner2 function
-eventEmitter.on('connection', listner2);
+   console.log(a);
+})
 
-var eventListeners = require('events').EventEmitter.listenerCount
-   (eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-// Remove the binding of listner1 function
-eventEmitter.removeListener('connection', listner1);
-console.log("Listner1 will not listen now.");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-console.log("Program Ended.");
+ab.emit('yoyoEvents', 'values');
