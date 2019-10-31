04c2f5f9e693d89494740fb91ae608a0c332d37a
Your Name <you@example.com>
2019-10-31 02:09:53 -0700

    video streaming

--- src/video-streaming/server.js
+++ src/video-streaming/server.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+const http = require('http');
 
+//const source = new EventSource();
 
 // route pages
 app.get('/', function (req, res) {
-  res.send('Main page is up and running!');
+    res.send('Main page is up and running!');
 });
 
-app.get('/user', function (req, res) {
-    const path = './assets/video.mp4';
-    
+app.get('/video', function (req, res) {
+    const path = '../../img/videoplayback.mp4';
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    var a = { a: req.headers.range }
+
+    const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4'
+
+    }
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res)
 });
 
 // what port to run server on
 app.listen(1111, function () {
-  console.log('server started on port 1111');
+    console.log('server started on port 1111');
 });
